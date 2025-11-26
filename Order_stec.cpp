0b9be7a9acc2e0fa03415f866ffc2ff30a9ce123
CybNNR <deliasvaraya@gmail.com>
2025-11-26 08:57:56 -0600

    Deque by order using stacks and vectors

---
+++ Order_stec.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<deque>
+#include<vector>
+#include<stack>
+
+int main(){
+    std::vector<int> v = {1,2,4,6,5,88,12,33};
+    std::deque d;
+    std::stack s;
+    
+    while(not s.empty()){
+        int x = s.top();
+        s.pop();
+        if(x % 2 == 0){
+            d.push_front(x*2-1);
+        }else{
+            d.push_back(-x+1);
+        }
+    }
+    while(not d.empty()){
+        int x = d.front();
+        d.pop_front();
+        std::cout<< x <<", ";
+    }
+}
